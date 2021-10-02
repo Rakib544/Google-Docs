fa4182d5849a81670670c3d5a9a14b11de8d2196
Rakib544 <md.rakib10122003@gmail.com>
2021-10-02 23:06:30 +0600

    editor setup complete

---
+++ src/components/TextEditor.js
@@ -0,0 +1,37 @@
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+import { useCallback } from "react";
+
+// toolbar customization
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "unordered" }],
+  ["bold", "italic", "underline"],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: {} }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+];
+
+const TextEditor = () => {
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    new Quill(editor, {
+      theme: "snow",
+      modules: {
+        toolbar: TOOLBAR_OPTIONS,
+      },
+    });
+  }, []);
+  return (
+    <div className="container" ref={wrapperRef}>
+      <p>TextEditor</p>
+    </div>
+  );
+};
+
+export default TextEditor;
